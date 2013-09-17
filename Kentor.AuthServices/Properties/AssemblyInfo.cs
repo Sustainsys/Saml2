c6962713619e13a2c3135b59e7804c21a655117f
Anders Abel <anders@abel.nu>
2013-09-17 10:54:41 +0200

    - Set up code analysis dictionary. - Added first embryo of Saml2AuthenticationModule

--- Kentor.AuthServices/Properties/AssemblyInfo.cs
+++ Kentor.AuthServices/Properties/AssemblyInfo.cs
@@ -1,16 +1,16 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kentor.AuthServices")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Saml2 Authentication for ASP.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kentor")]
 [assembly: AssemblyProduct("Kentor.AuthServices")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright Kentor © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
+[assembly: CLSCompliant(true)]
---
+++ Kentor.AuthServices/Saml2AuthenticationModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+
+namespace Kentor.AuthServices
+{
+    /// <summary>
+    /// Http Module for SAML2 authentication. The module hijacks the 
+    /// ~/Saml2AuthenticationModule/ path of the http application to provide 
+    /// authentication services.
+    /// </summary>
+    public class Saml2AuthenticationModule : IHttpModule
+    {
+        /// <summary>
+        /// Init the module and subscribe to events.
+        /// </summary>
+        /// <param name="context"></param>
+        public void Init(HttpApplication context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            context.BeginRequest += OnBeginRequest;
+        }
+
+        /// <summary>
+        /// Begin request handler that captures all traffic to ~/Saml2AuthenticationModule/
+        /// </summary>
+        /// <param name="sender">The http application.</param>
+        /// <param name="e">Ignored</param>
+        protected virtual void OnBeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// IDisposable implementation.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            // Deliberately do nothing, unsubscribing from events is not,
+            // needed by the IIS model. Trying to do so throws exceptions.
+        }
+    }
+}
