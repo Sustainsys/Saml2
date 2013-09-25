57a056e696f2f820d1a24da0828b05af15710199
Anders Abel <anders@abel.nu>
2013-09-25 10:01:16 +0200

    Added Saml2PostBinding

---
+++ Kentor.AuthServices.Tests/Saml2PostBindingTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Web;
+using FluentAssertions;
+using System.Collections.Specialized;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class Saml2PostBindingTests
+    {
+        [TestMethod]
+        public void Saml2PostBinding_CanUnbind_FalseOnNotPost()
+        {
+            var r = Substitute.For<HttpRequestBase>();
+            r.HttpMethod.Returns("GET");
+
+            new Saml2PostBinding().CanUnbind(r).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Saml2PostBinding_CanUnbind_FalseOnNoSamlRespose()
+        {
+            var r = Substitute.For<HttpRequestBase>();
+            r.HttpMethod.Returns("POST");
+            r.Form.Returns(new NameValueCollection());
+
+            new Saml2PostBinding().CanUnbind(r).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Saml2PostBinding_CanUnbind_True()
+        {
+            var r = Substitute.For<HttpRequestBase>();
+            r.HttpMethod.Returns("POST");
+            r.Form.Returns(new NameValueCollection() { { "SAMLResponse", "someData" } });
+
+            new Saml2PostBinding().CanUnbind(r).Should().BeTrue();
+        }
+    }
+}
---
+++ Kentor.AuthServices/Saml2PostBinding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Kentor.AuthServices
+{
+    class Saml2PostBinding : Saml2Binding
+    {
+        public override CommandResult Bind(Saml2AuthenticationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanUnbind(System.Web.HttpRequestBase request)
+        {
+            return request.HttpMethod == "POST" 
+                && request.Form.AllKeys.Contains("SAMLResponse");
+        }
+    }
+}
