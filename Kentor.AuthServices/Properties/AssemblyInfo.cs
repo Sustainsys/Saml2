813d182a67575cb519a344b8ddb056625a255ffc
Anders Abel <anders@abel.nu>
2013-09-30 21:01:19 +0200

    Updating version number after release

--- Kentor.AuthServices/Properties/AssemblyInfo.cs
+++ Kentor.AuthServices/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyFileVersion("0.1.1")]
 
 [assembly: CLSCompliant(true)]
 
