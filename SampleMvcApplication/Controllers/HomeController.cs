3660eb9fad40841c0cc840d42c33b29849ccc929
Anders Abel <anders@abel.nu>
2013-10-31 21:37:31 +0100

    Prepared Mvc Sample app and removed release build conf from sample projects.

--- SampleMvcApplication/Controllers/HomeController.cs
+++ SampleMvcApplication/Controllers/HomeController.cs
@@ -13,17 +13,9 @@ namespace SampleMvcApplication.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [Authorize]
+        public ActionResult Secure()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
