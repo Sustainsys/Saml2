b66f46ad059459f1f869474d74e218633d030cae
Anders Abel <anders@abel.nu>
2013-11-02 23:08:23 +0100

    Basic layout done. Added less and bundle support. I'm definitely not a design/CSS/LESS guy. Don't laugh.

---
+++ Kentor.AuthServices.StubIdp/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using Kentor.AuthServices.StubIdp.App_Start;
+
+namespace Kentor.AuthServices.StubIdp
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new Bundle("~/Content/css")
+                {
+                    Transforms = { new LessTransform() }
+                }
+                .Include(
+                "~/Content/normalize.css",
+                "~/Content/site.less"));
+        }
+    }
+}
--- Kentor.AuthServices.StubIdp/Global.asax.cs
+++ Kentor.AuthServices.StubIdp/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Kentor.AuthServices.StubIdp
@@ -13,6 +14,7 @@ namespace Kentor.AuthServices.StubIdp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
