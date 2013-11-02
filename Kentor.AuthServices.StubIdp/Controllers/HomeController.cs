ab21d9cda1f35a0709872ab03e1d8c1dd02dd44c
Anders Abel <anders@abel.nu>
2013-11-02 20:38:54 +0100

    Created skeleton index page.

---
+++ Kentor.AuthServices.StubIdp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kentor.AuthServices.StubIdp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
