d5cd41b8bbbb855a3b09ddf436e5802d92a4c48a
Anders Abel <anders@abel.nu>
2013-11-02 20:31:50 +0100

    Made StubIdp project use common version info.

--- Kentor.AuthServices.StubIdp/Properties/AssemblyInfo.cs
+++ Kentor.AuthServices.StubIdp/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("042b6d1c-a933-43dc-a5e1-2cc0d3b52d8b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
