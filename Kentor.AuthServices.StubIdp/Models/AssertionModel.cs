a3c8a5217a901dc4138f04023b0e4ffaf9a1a915
Anders Abel <anders@abel.nu>
2013-11-30 22:31:12 +0100

    Adding assertion form to start page. - Added assertionmodel and set up a basic form.

---
+++ Kentor.AuthServices.StubIdp/Models/AssertionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kentor.AuthServices.StubIdp.Models
+{
+    public class AssertionModel
+    {
+        [Required]
+        [Display(Name="Assertion Consumer Url")]
+        public string AssertionConsumerUrl { get; set; }
+
+        [Display(Name="Assertion NameId")]
+        public string NameId { get; set; }
+    }
+}
