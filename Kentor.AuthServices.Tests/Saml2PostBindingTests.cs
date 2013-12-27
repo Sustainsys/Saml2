8ef3199d4f186964dbb4332b1489c1a4f7882b4b
Anders Abel <anders@abel.nu>
2013-12-28 00:00:20 +0100

    Added support for sending messages over Saml2 HttpPostBinding

--- Kentor.AuthServices.Tests/Saml2PostBindingTests.cs
+++ Kentor.AuthServices.Tests/Saml2PostBindingTests.cs
@@ -17,7 +17,7 @@ namespace Kentor.AuthServices.Tests
         {
             var r = Substitute.For<HttpRequestBase>();
             r.HttpMethod.Returns("POST");
-            r.Form.Returns(new NameValueCollection() { { "SAMLResponse", encodedResponse} });
+            r.Form.Returns(new NameValueCollection() { { "SAMLResponse", encodedResponse } });
             return r;
         }
 
@@ -32,7 +32,7 @@ namespace Kentor.AuthServices.Tests
         [TestMethod]
         public void Saml2PostBinding_Unbind_ReadsSaml2ResponseId()
         {
-            string response = 
+            string response =
             @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
             ID = ""id1"" Version=""2.0"" IssueInstant=""2013-01-01T01:04:01Z"">
                 <saml2p:Status>
@@ -43,6 +43,46 @@ namespace Kentor.AuthServices.Tests
             var r = CreateRequest(Convert.ToBase64String(Encoding.UTF8.GetBytes(response)));
 
             Saml2Binding.Get(Saml2BindingType.HttpPost).Unbind(r).Id.Should().Be("id1");
+        }
+
+        [TestMethod]
+        public void Saml2PostBinding_Bind()
+        {
+            var xmlData = "<root><content>data</content></root>";
+            var request = Substitute.For<ISaml2Message>();
+            request.ToXml().Returns(xmlData);
+            request.DestinationUri.Returns(new Uri("http://www.example.com/acs"));
+
+            var subject = Saml2Binding.Get(Saml2BindingType.HttpPost).Bind(request);
+
+            var expected = new CommandResult()
+            {
+                Body = @"<!DOCTYPE html>
+<html>
+<body onload=""document.forms[0].submit()"">
+<noscript>
+<p>
+<strong>Note:</strong> Since your browser does not support JavaScript, 
+you must press the Continue button once to proceed.
+</p>
+</noscript>
+<form action=""http://www.example.com/acs"" 
+method=""post"">
+<div>
+<input type=""hidden"" name=""SAMLRequest"" 
+value=""PHJvb3Q+PGNvbnRlbnQ+ZGF0YTwvY29udGVudD48L3Jvb3Q+""/>
+</div>
+<noscript>
+<div>
+<input type=""submit"" value=""Continue""/>
+</div>
+</noscript>
+</form>
+</body>
+</html>"
+            };
+
+            subject.ShouldBeEquivalentTo(expected);
         }
     }
 }
--- Kentor.AuthServices/Saml2PostBinding.cs
+++ Kentor.AuthServices/Saml2PostBinding.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -20,5 +21,43 @@ namespace Kentor.AuthServices
 
             return Saml2Response.Read(xml);            
         }
+
+        public override CommandResult Bind(ISaml2Message request)
+        {
+            var encodedXml = Convert.ToBase64String(
+                Encoding.UTF8.GetBytes(request.ToXml()));
+
+            var cr = new CommandResult()
+            {
+                Body = String.Format(CultureInfo.InvariantCulture, PostHtmlFormatStrign, 
+                request.DestinationUri, encodedXml)
+            };
+
+            return cr;
+        }
+
+        private const string PostHtmlFormatStrign = @"<!DOCTYPE html>
+<html>
+<body onload=""document.forms[0].submit()"">
+<noscript>
+<p>
+<strong>Note:</strong> Since your browser does not support JavaScript, 
+you must press the Continue button once to proceed.
+</p>
+</noscript>
+<form action=""{0}"" 
+method=""post"">
+<div>
+<input type=""hidden"" name=""SAMLRequest"" 
+value=""{1}""/>
+</div>
+<noscript>
+<div>
+<input type=""submit"" value=""Continue""/>
+</div>
+</noscript>
+</form>
+</body>
+</html>";
     }
 }
