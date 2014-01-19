1e9b5bd99dd7979bcf1ffaecb70c7fb9b3bdf284
Anders Abel <anders@abel.nu>
2014-01-19 17:15:31 +0100

    Added tests for AuthServicesException base class.

---
+++ Kentor.AuthServices.Tests/AuthServicesExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class AuthServicesExceptionTests
+    {
+        private class ConcreteAuthServicesException : AuthServicesException
+        {
+            public ConcreteAuthServicesException()
+                : base()
+            { }
+        }
+
+        [TestMethod]
+        public void TestBasicCtor()
+        {
+            ExceptionTestHelpers.TestDefaultCtor<ConcreteAuthServicesException>();
+        }
+    }
+}
---
+++ Kentor.AuthServices.Tests/ExceptionTestHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices.Tests
+{
+    /// <summary>
+    /// Helper class for testing standard exception constructors.
+    /// </summary>
+    static class ExceptionTestHelpers
+    {
+        public static void TestDefaultCtor<TException>()
+            where TException: Exception, new()
+        {
+            new TException();
+
+            // Deliberately no asserts - just testing that default ctor can
+            // be run and exists.
+        }
+
+    }
+}
