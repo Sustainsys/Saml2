bd12bc2465f232132f897ced3b0c5a023e743880
Anders Abel <anders@abel.nu>
2014-01-21 19:42:40 +0100

    Fixed code coverage for tests for Enumerator. - Refactored and made the GenericEnmumeratorAdapter a private class.

---
+++ Kentor.AuthServices.Tests/EnumeratorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using FluentAssertions;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class EnumeratorTests
+    {
+        [TestMethod]
+        public void Enumerator_AsGeneric()
+        {
+            IEnumerable src = new int[] { 1, 2 };
+
+            var subject = src.GetEnumerator().AsGeneric<int>();
+
+            subject.MoveNext().Should().BeTrue();
+            subject.Current.Should().Be(1);
+            subject.MoveNext().Should().BeTrue();
+            subject.Current.Should().Be(2);
+            subject.MoveNext().Should().BeFalse();
+
+            subject.Reset();
+            subject.MoveNext().Should().BeTrue();
+            subject.Current.Should().Be(1);
+
+            ((IEnumerator)subject).Current.Should().Be(1);
+        }
+    }
+}
--- Kentor.AuthServices/Enumerator.cs
+++ Kentor.AuthServices/Enumerator.cs
@@ -9,7 +9,45 @@ namespace Kentor.AuthServices
 {
     static class Enumerator
     {
-        public static GenericEnumeratorAdapter<T> AsGeneric<T>(this IEnumerator source)
+        class GenericEnumeratorAdapter<T> : IEnumerator<T>
+        {
+            private readonly IEnumerator source;
+
+            public GenericEnumeratorAdapter(IEnumerator source)
+            {
+                this.source = source;
+            }
+
+            public T Current
+            {
+                get { return (T)source.Current; }
+            }
+
+            public void Dispose()
+            {
+                // Non generic IEnumerator doesn't implement IDisposable so do nothing.
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get
+                {
+                    return source.Current;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                return source.MoveNext();
+            }
+
+            public void Reset()
+            {
+                source.Reset();
+            }
+        }
+
+        public static IEnumerator<T> AsGeneric<T>(this IEnumerator source)
         {
             return new GenericEnumeratorAdapter<T>(source);
         }
