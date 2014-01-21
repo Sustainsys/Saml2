db30dd01ad50973ea55ea38e7a6374569549c47e
Anders Abel <anders@abel.nu>
2014-01-21 21:42:55 +0100

    Covering tests for ReturnRequestedIssuerNameRegistry.

---
+++ Kentor.AuthServices.Tests/ReturnRequestedIssuerNameRegistryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class ReturnRequestedIssuerNameRegistryTests
+    {
+        [TestMethod]
+        public void ReturnRequestedIssuerNameRegistry_GetIssuerNameRequested()
+        {
+            var name = "SomeName";
+
+            new ReturnRequestedIssuerNameRegistry().GetIssuerName(
+                null, name).Should().Be(name);
+        }
+
+        [TestMethod]
+        public void ReturnRequestedIssuerNameRegistry_GetIssuerName()
+        {
+            Action a = () => new ReturnRequestedIssuerNameRegistry().GetIssuerName(null);
+
+            a.ShouldThrow<NotImplementedException>();
+        }
+    }
+}
