9231192daa1319e8fe770be6143d990b0581d968
Anders Abel <anders@abel.nu>
2014-01-22 22:33:59 +0100

    Covering tests for Saml2RedirectBinding.

---
+++ Kentor.AuthServices.Tests/Saml2RedirectBindingTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class Saml2RedirectBindingTests
+    {
+        [TestMethod]
+        public void Saml2RedirectBinding_Nullcheck()
+        {
+            Action a = () => Saml2Binding.Get(Saml2BindingType.HttpRedirect)
+                .Bind(null);
+
+            a.ShouldThrow<ArgumentNullException>().WithMessage(
+                "Value cannot be null.\r\nParameter name: message");
+        }
+    }
+}
