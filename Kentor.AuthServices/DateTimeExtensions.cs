84c01734b859ba309bf1e5451510fff17b27c513
Anders Abel <anders@abel.nu>
2014-03-02 18:31:38 +0100

    Added extension method to format dates

---
+++ Kentor.AuthServices/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices
+{
+    static class DateTimeExtensions
+    {
+        public static string ToSaml2DateTimeString(this DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime().ToString("s", CultureInfo.InvariantCulture) + "Z";
+        }
+    }
+}
