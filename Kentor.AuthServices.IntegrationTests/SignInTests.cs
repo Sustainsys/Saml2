c20bdd57f5fa086d21198cd515e806500c4606d9
Anders Abel <anders@abel.nu>
2014-04-18 16:46:31 +0200

    Test on unsolicited SigIn
    - Works, but timing issues should be improved.
    - Should fix test playlist to be able to run unit tests separately.

---
+++ Kentor.AuthServices.IntegrationTests/SignInTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAutomation;
+
+namespace Kentor.AuthServices.IntegrationTests
+{
+    [TestClass]
+    public class SignInTests : FluentTest
+    {
+        [TestInitialize]
+        public void SignInTestsInitialize()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+            Settings.DefaultWaitTimeout = TimeSpan.FromSeconds(5);
+            Settings.DefaultWaitUntilThreadSleep = TimeSpan.FromSeconds(5);
+            Settings.DefaultWaitUntilTimeout = TimeSpan.FromSeconds(5);
+        }
+
+        [TestMethod]
+        public void SignIn_Unsolicited()
+        {
+            I.Open("http://localhost:52071/")
+                .Click("#main form button")
+                .Click("a[href=\"/Home/Secure\"]")
+                .Assert.Text("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier - JohnDoe").In(".body-content ul li:first-child");
+
+            I.Click("a[href=\"/AuthServices/SignOut\"");
+        }
+    }
+}
