33b7c8fd0d553ab5daf7c37e34be9cdb25bfd2a7
Anders Abel <anders@abel.nu>
2014-05-12 10:36:24 +0200

    Updated version number to 0.4.0 for release.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 // An odd patch number is a development version, an even patch number is
 // a relased version.
 
-[assembly: AssemblyVersion("0.3.1")]
-[assembly: AssemblyFileVersion("0.3.1")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
