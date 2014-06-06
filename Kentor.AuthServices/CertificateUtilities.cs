cd7626883fe53296eff82f7f815ac857b1d2ac78
George Heeres <gheeres@gmail.com>
2014-06-05 23:11:18 -0500

    Remove unused using statements.

--- Kentor.AuthServices/CertificateUtilities.cs
+++ Kentor.AuthServices/CertificateUtilities.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kentor.AuthServices
 {
