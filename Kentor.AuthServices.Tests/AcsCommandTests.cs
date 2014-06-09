2e1767b301ba115123727f35e9327e8181a63233
Albin Sunnanbo <albin@sunnanbo.com>
2014-06-09 16:24:59 +0200

    Fix code coverage for AcsCommand.Location

--- Kentor.AuthServices.Tests/AcsCommandTests.cs
+++ Kentor.AuthServices.Tests/AcsCommandTests.cs
@@ -9,6 +9,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Web;
 using System.Xml;
+using System.Linq;
 
 namespace Kentor.AuthServices.Tests
 {
@@ -97,6 +98,58 @@ namespace Kentor.AuthServices.Tests
                 Principal = new ClaimsPrincipal(ids),
                 HttpStatusCode = HttpStatusCode.SeeOther,
                 Location = new Uri("http://localhost/LoggedIn")
+            };
+
+            new AcsCommand().Run(r).ShouldBeEquivalentTo(expected,
+                opt => opt.IgnoringCyclicReferences());
+        }
+
+        [TestMethod]
+        [NotReRunnable]
+        public void AcsCommand_Run_WithReturnUrl_SuccessfulResult()
+        {
+            var idp = IdentityProvider.ConfiguredIdentityProviders.First().Value;
+            var request = idp.CreateAuthenticateRequest(new Uri("http://localhost/testUrl.aspx"));
+
+            var r = Substitute.For<HttpRequestBase>();
+            r.HttpMethod.Returns("POST");
+
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+                xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                ID = ""AcsCommand_Run_WithReturnUrl_SuccessfulResult"" InResponseTo = """ + request.Id + @""" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z"">
+                <saml2:Issuer>
+                    https://idp.example.com
+                </saml2:Issuer>
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                <saml2:Assertion
+                Version=""2.0"" ID=""Saml2Response_GetClaims_CreateIdentity_Assertion2""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>SomeUser</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                    <saml2:Conditions NotOnOrAfter=""2100-01-01T00:00:00Z"" />
+                </saml2:Assertion>
+            </saml2p:Response>";
+
+            var formValue = Convert.ToBase64String(Encoding.UTF8.GetBytes(
+                SignedXmlHelper.SignXml(response)));
+
+            r.Form.Returns(new NameValueCollection() { { "SAMLResponse", formValue } });
+
+            var ids = new ClaimsIdentity[] { new ClaimsIdentity("Federation"), new ClaimsIdentity("ClaimsAuthenticationManager") };
+            ids[0].AddClaim(new Claim(ClaimTypes.NameIdentifier, "SomeUser", null, "https://idp.example.com"));
+            ids[1].AddClaim(new Claim(ClaimTypes.Role, "RoleFromClaimsAuthManager", null, "ClaimsAuthenticationManagerMock"));
+
+            var expected = new CommandResult()
+            {
+                Principal = new ClaimsPrincipal(ids),
+                HttpStatusCode = HttpStatusCode.SeeOther,
+                Location = new Uri("http://localhost/testUrl.aspx")
             };
 
             new AcsCommand().Run(r).ShouldBeEquivalentTo(expected,
