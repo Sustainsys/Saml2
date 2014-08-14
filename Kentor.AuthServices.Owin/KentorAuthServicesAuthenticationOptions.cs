09501a590acca360a745e74dcf06418302d0a5e3
Anders Abel <anders@abel.nu>
2014-08-14 15:29:26 +0200

    Added setting of authentication type caption to Owin options. - Without a caption, the external login isn't shown on the login page.

--- Kentor.AuthServices.Owin/KentorAuthServicesAuthenticationOptions.cs
+++ Kentor.AuthServices.Owin/KentorAuthServicesAuthenticationOptions.cs
@@ -15,10 +15,13 @@ namespace Kentor.AuthServices.Owin
         /// <summary>
         /// Constructor
         /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "KentorAuthServices")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.Owin.Security.AuthenticationDescription.set_Caption(System.String)")]
         public KentorAuthServicesAuthenticationOptions()
             : base(Constants.DefaultAuthenticationType)
         {
             AuthenticationMode = AuthenticationMode.Passive;
+            Description.Caption = Constants.DefaultAuthenticationType;
         }
     }
 }
---
+++ Kentor.AuthServices.Tests/KentorAuthServicesAuthenticationOptionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kentor.AuthServices.Owin;
+using FluentAssertions;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class KentorAuthServicesAuthenticationOptionsTests
+    {
+        [TestMethod]
+        public void KentorAuthServicesAuthenticationOptions_SetsDefaultDescriptionCaption()
+        {
+            var subject = new KentorAuthServicesAuthenticationOptions();
+
+            subject.Description.Caption.Should().Be(Constants.DefaultAuthenticationType);
+        }
+    }
+}
