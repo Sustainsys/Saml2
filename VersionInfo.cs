24453e0be2d4ba528783c7a4eb5ede407fedda9f
Anders Abel <anders@abel.nu>
2014-08-19 14:08:39 +0200

    Updated version number to 0.5.1 for development.

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 // An odd patch number is a development version, an even patch number is
 // a relased version.
 
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("0.5.1")]
+[assembly: AssemblyFileVersion("0.5.1")]
