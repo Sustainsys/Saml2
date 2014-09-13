dabc83268768fe46cce06a325f27e37e82ad916e
Anders Abel <anders@abel.nu>
2014-09-13 09:20:53 +0200

    Refactored metadata generation to use MetadataSerializer. - Added a custom CacheAwareMetadataSerializer that handles the cacheDuration attribute. - Fixes #106.

---
+++ Kentor.AuthServices/CacheAwareMetadataSerializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IdentityModel.Metadata;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Kentor.AuthServices
+{
+    class CacheAwareMetadataSerializer : MetadataSerializer
+    {
+        int cacheDuration;
+
+        public CacheAwareMetadataSerializer(int cacheDuration)
+        {
+            this.cacheDuration = cacheDuration;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1062:Validate arguments of public methods", MessageId = "0", Justification="Method is only called by base class no validation needed.")]
+        protected override void WriteCustomAttributes<T>(XmlWriter writer, T source)
+        {
+            if (typeof(T) == typeof(EntityDescriptor))
+            {
+                writer.WriteAttributeString("cacheDuration", cacheDuration.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
---
+++ Kentor.AuthServices/MetadatabaseExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Kentor.AuthServices.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Metadata;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Kentor.AuthServices
+{
+    /// <summary>
+    /// Extensions for Metadatabase.
+    /// </summary>
+    public static class MetadataBaseExtensions
+    {
+        /// <summary>
+        /// Use a MetadataSerializer to create an XML string out of metadata.
+        /// </summary>
+        /// <param name="metadata">Metadata to serialize.</param>
+        /// <param name="cacheDuration">Cache duration value to include in output.</param>
+        /// <returns>string with metadata contents.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
+        public static string ToXmlString(this MetadataBase metadata, int cacheDuration)
+        {
+            var serializer = new CacheAwareMetadataSerializer(cacheDuration);
+
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteMetadata(stream, metadata);
+
+                using (var reader = new StreamReader(stream))
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
