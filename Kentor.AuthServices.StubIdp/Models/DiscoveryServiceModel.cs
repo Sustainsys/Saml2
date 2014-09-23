8b9e80b386ef510464e5fb010cad66bb47381249
Anders Abel <anders@abel.nu>
2014-09-23 22:37:30 +0200

    Added discovery service to stubidp.

---
+++ Kentor.AuthServices.StubIdp/Models/DiscoveryServiceModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kentor.AuthServices.StubIdp.Models
+{
+    public class DiscoveryServiceModel
+    {
+        public DiscoveryServiceModel()
+        {
+            returnIDParam = "entityID";
+            SelectedIdp = UrlResolver.MetadataUrl.ToString();
+        }
+
+        [Display(Name="SP Entity ID")]
+        public string entityID { get; set; }
+
+        [Display(Name="Return URL to SP")]
+        [Required]
+        public string @return { get; set; }
+
+        [Display(Name="Return ID Param")]
+        [Required]
+        public string returnIDParam { get; set; }
+
+        public bool isPassive { get; set; }
+
+        [Display(Name="Entity ID of Selected Idp")]
+        [Required]
+        public string SelectedIdp { get; set; }
+    }
+}
