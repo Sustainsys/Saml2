fe222419d3bd8494ca2583ae78283c6994b392ef
cornem <corne@holmwoods.eu>
2015-04-28 10:54:02 +0200

    Add

--- SampleOwinApplication/Global.asax.cs
+++ SampleOwinApplication/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,6 +13,7 @@ namespace SampleOwinApplication
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
