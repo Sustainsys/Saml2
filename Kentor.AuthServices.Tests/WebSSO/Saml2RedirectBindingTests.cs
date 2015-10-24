d97a8f2a2c8db3df0cccca5b98d4d8f0b51eb0a1
Anders Abel <anders@abel.nu>
2015-10-24 23:13:45 +0200

    Replaced in-source line endings with explicit. - Test was failing for some people depending on line   ending settings in their environment. Use explicit   line endings to make it independent of the line ending   settings of the environment. - Changed the url to end in sso instead of acs - an   AuthenRequest is sent to an sso service on an idp.

--- Kentor.AuthServices.Tests/WebSSO/Saml2RedirectBindingTests.cs
+++ Kentor.AuthServices.Tests/WebSSO/Saml2RedirectBindingTests.cs
@@ -38,30 +38,29 @@ namespace Kentor.AuthServices.Tests.WebSso
         public void Saml2RedirectBinding_Bind()
         {
             // Example from http://en.wikipedia.org/wiki/SAML_2.0#HTTP_Redirect_Binding
-            var xmlData = @"<?xml version=""1.0"" encoding=""UTF-8""?>
-<samlp:AuthnRequest
-  xmlns:samlp=""urn:oasis:names:tc:SAML:2.0:protocol""
-  xmlns:saml=""urn:oasis:names:tc:SAML:2.0:assertion""
-  ID=""aaf23196-1773-2113-474a-fe114412ab72""
-  Version=""2.0""
-  IssueInstant=""2004-12-05T09:21:59Z""
-  AssertionConsumerServiceIndex=""0""
-  AttributeConsumingServiceIndex=""0"">
-  <saml:Issuer>https://sp.example.com/SAML2</saml:Issuer>
-  <samlp:NameIDPolicy
-    AllowCreate=""true""
-    Format=""urn:oasis:names:tc:SAML:2.0:nameid-format:transient""/>
-</samlp:AuthnRequest>
-";
+            var xmlData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+                + "<samlp:AuthnRequest\r\n"
+                + "  xmlns:samlp=\"urn:oasis:names:tc:SAML:2.0:protocol\"\r\n"
+                + "  xmlns:saml=\"urn:oasis:names:tc:SAML:2.0:assertion\"\r\n"
+                + "  ID=\"aaf23196-1773-2113-474a-fe114412ab72\"\r\n"
+                + "  Version=\"2.0\"\r\n"
+                + "  IssueInstant=\"2004-12-05T09:21:59Z\"\r\n"
+                + "  AssertionConsumerServiceIndex=\"0\"\r\n"
+                + "  AttributeConsumingServiceIndex=\"0\">\r\n"
+                + "  <saml:Issuer>https://sp.example.com/SAML2</saml:Issuer>\r\n"
+                + "  <samlp:NameIDPolicy\r\n"
+                + "    AllowCreate=\"true\"\r\n"
+                + "    Format=\"urn:oasis:names:tc:SAML:2.0:nameid-format:transient\"/>\r\n"
+                + "</samlp:AuthnRequest>\r\n";
 
             var serializedData = "fZFfa8IwFMXfBb9DyXvaJtZ1BqsURRC2Mabbw95ivc5Am3TJrXPffmmLY3%2FA15Pzuyf33On8XJXBCaxTRmeEhTEJQBdmr%2FRbRp63K3pL5rPhYOpkVdYib%2FCon%2BC9AYfDQRB4WDvRvWWksVoY6ZQTWlbgBBZik9%2FfCR7GorYGTWFK8pu6DknnwKL%2FWEetlxmR8sBHbHJDWZqOKGdsRJM0kfQAjCUJ43KX8s78ctnIz%2Blp5xpYa4dSo1fjOKGM03i8jSeCMzGevHa2%2FBK5MNo1FdgN2JMqPLmHc0b6WTmiVbsGoTf5qv66Zq2t60x0wXZ2RKydiCJXh3CWVV1CWJgqanfl0%2Bin8xutxYOvZL18NKUqPlvZR5el%2BVhYkAgZQdsA6fWVsZXE63W2itrTQ2cVaKV2CjSSqL1v9P%2FAXv4C";
-            var destinationUrl = new Uri("http://www.example.com/acs");
+            var destinationUrl = new Uri("http://www.example.com/sso");
 
             var subject = Saml2Binding.Get(Saml2BindingType.HttpRedirect).Bind(xmlData, destinationUrl, null);
 
             var expected = new CommandResult()
             {
-                Location = new Uri("http://www.example.com/acs?SAMLRequest=" + serializedData),
+                Location = new Uri("http://www.example.com/sso?SAMLRequest=" + serializedData),
                 HttpStatusCode = System.Net.HttpStatusCode.SeeOther,
             };
 
@@ -77,7 +76,6 @@ namespace Kentor.AuthServices.Tests.WebSso
         public void Saml2RedirectBinding_Unbind()
         {
             // Example from http://en.wikipedia.org/wiki/SAML_2.0#HTTP_Redirect_Binding
-
             var xmlData = @"<?xml version=""1.0"" encoding=""UTF-8""?>
 <samlp:AuthnRequest
   xmlns:samlp=""urn:oasis:names:tc:SAML:2.0:protocol""
