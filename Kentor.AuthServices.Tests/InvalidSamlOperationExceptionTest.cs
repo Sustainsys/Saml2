7dfac5c3fa9355dccc91842a5b7e0d2ef66d454e
Marcin Rybacki <rybacki.marcin@gmail.com>
2015-12-30 12:21:04 +0100

    Adding secondary level status code for saml response as defined in 3.2.2.2 or the Assertions and Protocols for the OASIS Security Assertion Markup Language - this adds additional string field for secondary status message, the field is intentionally a string because specification says this status can change in future releases - additional exception has been added to get the statuscode and secondary status code if the STS response is other than Success. It may help to handle those situations outside the module.

---
+++ Kentor.AuthServices.Tests/InvalidSamlOperationExceptionTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Kentor.AuthServices.Tests
+{
+    [TestClass]
+    public class InvalidSamlOperationExceptionTest
+    {
+        [TestMethod]
+        public void InvalidSamlOperationException_DefaultCtor()
+        {
+            ExceptionTestHelpers.TestDefaultCtor<InvalidSamlOperationException>();
+        }
+
+        [TestMethod]
+        public void InvalidSamlOperationException_SerializationCtor()
+        {
+            ExceptionTestHelpers.TestSerializationCtor<InvalidSamlOperationException>();
+        }
+
+        [TestMethod]
+        public void InvalidSamlOperationException_StringCtor()
+        {
+            var msg = "Message!";
+            var subject = new InvalidSamlOperationException(msg);
+
+            subject.Message.Should().Be(msg);
+        }
+        [TestMethod]
+        public void InvalidSamlOperationException_StringInnerExCtor()
+        {
+            var msg = "Message!";
+            var inner = new Exception();
+            var subject = new InvalidSamlOperationException(msg, inner);
+
+            subject.Message.Should().Be(msg);
+            subject.InnerException.Should().Be(inner);
+        }
+
+    }
+}
