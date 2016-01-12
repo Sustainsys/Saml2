2a7ac64214c9dd0c2b4cdff571c1319b8c8f5cfc
Anders Abel <anders@abel.nu>
2016-01-12 10:51:35 +0100

    Added secure RelayState generator

---
+++ Kentor.AuthServices/RelayStateGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices
+{
+    /// <summary>
+    /// Generator of relay state for correlating requests.
+    /// </summary>
+    static class RelayStateGenerator
+    {
+        private static RNGCryptoServiceProvider random =
+            new RNGCryptoServiceProvider();
+
+        /// <summary>
+        /// Create a unique random string that with a cryptographically secure
+        /// random functions.
+        /// </summary>
+        /// <returns>Random string 56-chars string</returns>
+        public static string CreateSecureKey()
+        {
+            var bytes = new byte[42];
+            random.GetBytes(bytes);
+
+            return Convert.ToBase64String(bytes)
+                .Replace('/', '-')
+                .Replace('+', '_');
+        }
+    }
+}
