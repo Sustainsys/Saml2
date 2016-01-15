48a663756a1b3f135e365e9ab390a2ab0b487891
Anders Abel <anders@abel.nu>
2016-01-15 14:02:44 +0100

    Creation of SOAP request.

---
+++ Kentor.AuthServices.Tests/Saml2P/Saml2SoapBindingTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Kentor.AuthServices.Saml2P;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+
+namespace Kentor.AuthServices.Tests.Saml2P
+{
+    [TestClass]
+    public class Saml2SoapBindingTests
+    {
+        [TestMethod]
+        public void Saml2SoapBinding_CreateSoapBody()
+        {
+            var message = "<payload>data</payload>";
+
+            var expected = XElement.Parse(
+@"<SOAP-ENV:Envelope
+    xmlns:SOAP-ENV=""http://schemas.xmlsoap.org/soap/envelope/"">
+    <SOAP-ENV:Body>
+        <payload>data</payload>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>");
+
+            var actual = XElement.Parse(Saml2SoapBinding.CreateSoapBody(message));
+
+            actual.ShouldBeEquivalentTo(expected, opt => opt.IgnoringCyclicReferences());
+        }
+    }
+}
---
+++ Kentor.AuthServices/SAML2P/Saml2SoapBinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices.Saml2P
+{
+    /// <summary>
+    /// Saml2 Soap binding implementation.
+    /// </summary>
+    /// <remarks>
+    /// This class does not follow the pattern of the other three bindings
+    /// (Redirect, POST and Artifact) because it does not use the front channel
+    /// with messages being passed over the user's browser.
+    /// </remarks>
+    public static class Saml2SoapBinding
+    {
+        const string soapFormatString = "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"><SOAP-ENV:Body>{0}</SOAP-ENV:Body></SOAP-ENV:Envelope>";
+
+        internal static string CreateSoapBody(string payLoad)
+        {
+            return string.Format(CultureInfo.InvariantCulture,
+                soapFormatString, payLoad);
+        }
+    }
+}
