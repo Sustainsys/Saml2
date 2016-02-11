c6140a64117e78c6844ebd938d253ff848ba45e3
Anders Abel <anders@abel.nu>
2016-02-11 09:48:43 +0100

    Added LogoutController to stubidp.

---
+++ Kentor.AuthServices.StubIdp/Controllers/LogoutController.cs
@@ -0,0 +1,30 @@
+﻿using Kentor.AuthServices.HttpModule;
+using Kentor.AuthServices.Saml2P;
+using Kentor.AuthServices.StubIdp.Models;
+using Kentor.AuthServices.WebSso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kentor.AuthServices.StubIdp.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public ActionResult Index()
+        {
+            var unbindResult = Saml2Binding.Get(Saml2BindingType.HttpRedirect)
+                .Unbind(Request.ToHttpRequestData(), null);
+
+            //var request = new Saml2LogoutRequest(unbindResult.Data);
+
+            var model = new LogoutModel
+            {
+
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Kentor.AuthServices.StubIdp/Models/LogoutModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kentor.AuthServices.StubIdp.Models
+{
+    public class LogoutModel
+    {
+        [Description("Incoming LogoutRequest")]
+        public string LogoutRequestXml { get; set; }
+
+        [Required]
+        [DisplayName("Single Logout Service Response Url")]
+        public string SingleLogoutResponseUrl { get; set; }
+    }
+}
