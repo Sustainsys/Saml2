5e44a9fb4af3c7eaaab5f1ad6dc62beb05b02355
Anders Abel <anders@abel.nu>
2016-03-30 21:39:21 +0200

    Fixed code coverage

--- Kentor.AuthServices.Tests/WebSSO/AcsCommandTests.cs
+++ Kentor.AuthServices.Tests/WebSSO/AcsCommandTests.cs
@@ -294,6 +294,60 @@ namespace Kentor.AuthServices.Tests.WebSso
         }
 
         [TestMethod]
+        public void AcsCommand_Run_WithReturnUrl_SuccessfulResult_NoConfigReturnUrl()
+        {
+            var idp = Options.FromConfiguration.IdentityProviders.Default;
+
+            var response =
+            @"<saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+                xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                ID = """ + MethodBase.GetCurrentMethod().Name + @""" InResponseTo = ""InResponseToId"" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z"">
+                <saml2:Issuer>
+                    https://idp.example.com
+                </saml2:Issuer>
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                <saml2:Assertion
+                Version=""2.0"" ID=""" + MethodBase.GetCurrentMethod().Name + @"_Assertion2""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>SomeUser</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                    <saml2:Conditions NotOnOrAfter=""2100-01-01T00:00:00Z"" />
+                </saml2:Assertion>
+            </saml2p:Response>";
+
+            var responseFormValue = Convert.ToBase64String
+                (Encoding.UTF8.GetBytes(SignedXmlHelper.SignXml(response)));
+            var relayStateFormValue = "rs1234";
+
+            var r = new HttpRequestData(
+                "POST",
+                new Uri("http://localhost"),
+                "/ModulePath",
+                new KeyValuePair<string, string[]>[]
+                {
+                    new KeyValuePair<string, string[]>("SAMLResponse", new string[] { responseFormValue }),
+                    new KeyValuePair<string, string[]>("RelayState", new string[] { relayStateFormValue })
+                },
+                new StoredRequestState(
+                    new EntityId("https://idp.example.com"),
+                    new Uri("http://localhost/testUrl.aspx"),
+                    new Saml2Id("InResponseToId"),
+                    null)
+                );
+
+            var options = StubFactory.CreateOptions();
+            ((SPOptions)options.SPOptions).ReturnUrl = null;
+
+            new AcsCommand().Invoking(c => c.Run(r, options))
+                .ShouldNotThrow();
+        }
+
+        [TestMethod]
         public void AcsCommand_Run_UnsolicitedResponse_ThrowsOnNoConfiguredReturnUrl()
         {
            var response =
