665cc73a52fb79c89af43dbfa8f562ff1cf41434
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2016-04-15 13:30:40 +0200

    Added comments to scoping provider.

--- Kentor.AuthServices/Configuration/ISaml2ScopingProvider.cs
+++ Kentor.AuthServices/Configuration/ISaml2ScopingProvider.cs
@@ -4,16 +4,13 @@ using Kentor.AuthServices.Saml2P;
 namespace Kentor.AuthServices.Configuration
 {
     /// <summary>
-    /// 
+    /// Extension point for providing scoping information to Saml2AuthenticationRequests.
     /// </summary>
     public interface ISaml2ScopingProvider
     {
         /// <summary>
-        /// 
+        /// Build the relevant scoping given the authentication request and relay data.
         /// </summary>
-        /// <param name="authenticationRequest"></param>
-        /// <param name="relayData"></param>
-        /// <returns></returns>
         Saml2Scoping GetScoping(Saml2AuthenticationRequest authenticationRequest, IDictionary<string, string> relayData);
     }
 }
--- Kentor.AuthServices/Configuration/SingleSaml2ScopingProvider.cs
+++ Kentor.AuthServices/Configuration/SingleSaml2ScopingProvider.cs
@@ -4,27 +4,24 @@ using Kentor.AuthServices.Saml2P;
 namespace Kentor.AuthServices.Configuration
 {
     /// <summary>
-    /// 
+    /// Applies a single static scoping to all autentication requests.
     /// </summary>
     public class SingleSaml2ScopingProvider : ISaml2ScopingProvider
     {
         private readonly Saml2Scoping _saml2Scoping;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="SingleSaml2ScopingProvider"/> class. 
         /// </summary>
-        /// <param name="saml2Scoping"></param>
+        /// <param name="saml2Scoping">The scoping that will always be applied.</param>
         public SingleSaml2ScopingProvider(Saml2Scoping saml2Scoping)
         {
             _saml2Scoping = saml2Scoping;
         }
 
         /// <summary>
-        /// 
+        /// Build the relevant scoping given the authentication request and relay data.
         /// </summary>
-        /// <param name="authenticationRequest"></param>
-        /// <param name="relayData"></param>
-        /// <returns></returns>
         public Saml2Scoping GetScoping(Saml2AuthenticationRequest authenticationRequest, IDictionary<string, string> relayData)
         {
             return _saml2Scoping;
