1bde556a89d7aa53d22fba4988778c59f873bcc3
SWS Solutions <info@sws-solutions.eu>
2016-04-19 12:20:52 +0200

    Fix unit test

--- Kentor.AuthServices.Tests/Saml2P/Saml2ArtifactResolveTests.cs
+++ Kentor.AuthServices.Tests/Saml2P/Saml2ArtifactResolveTests.cs
@@ -34,7 +34,7 @@ namespace Kentor.AuthServices.Tests.Saml2P
     ID = ""_6c3a4f8b9c2d"" Version = ""2.0""
     IssueInstant = ""2004-01-21T19:00:49Z"" >
     <saml2:Issuer>http://sp.example.com</saml2:Issuer>
-    <saml2:Artifact>MyArtifact</saml2:Artifact>
+    <saml2p:Artifact>MyArtifact</saml2p:Artifact>
  </saml2p:ArtifactResolve>");
 
             // Set generated expected values to the actual.
