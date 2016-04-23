f2701bc48f6b548d37cce3f98e6d52f0591880d0
Jimmy Tønners <jimmy@baguit.com>
2016-04-23 10:12:40 +0200

    Introduced notification mechanism on IOptions. No tests yet.

---
+++ Kentor.AuthServices/Configuration/KentorAuthServicesNotifications .cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Kentor.AuthServices.Saml2P;
+
+namespace Kentor.AuthServices.Configuration
+{
+    public class KentorAuthServicesNotifications
+    {
+        public KentorAuthServicesNotifications()
+        {
+            AuthenticationRequestCreated = (request, provider, dictionary) => { };
+        }
+        public AuthenticationRequestCreatedNotification  AuthenticationRequestCreated { get; set; }
+    }
+
+    public delegate void AuthenticationRequestCreatedNotification(Saml2AuthenticationRequest saml2AuthenticationRequest, IdentityProvider identityProvider, IDictionary<string,string> dictionary);
+
+    /*
+    public class AuthenticationRequestCreatedNotification :
+        KentorAuthServicesNotification<Saml2AuthenticationRequest, IdentityProvider, IDictionary<string, string>>
+    {
+        public AuthenticationRequestCreatedNotification(Action<Saml2AuthenticationRequest, IdentityProvider, IDictionary<string, string>> handler) : base(handler)
+        {
+        }
+    }
+
+    public class KentorAuthServicesNotification<T1,T2,T3>
+    {
+        private readonly Action<T1, T2, T3> _handler;
+
+        protected KentorAuthServicesNotification(Action<T1, T2, T3> handler)
+        {
+            _handler = handler;
+        }
+
+        public void Invoke(T1 type1, T2 type2, T3 type3)
+        {
+            _handler(type1, type2, type3);
+        }
+    }
+    */
+}
