86804820ea70a7ce2fb68fd1d9b51ec0a83a72f8
Kyle Spearrin <kspearrin@imobile3.com>
2016-05-20 17:32:44 -0400

    Created new Kentor.AuthServices.AspNetCore project. Started setting up options, middleware, defaults, and handler.

---
+++ Kentor.AuthServices.AspNetCore/KentorAuthServicesHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+
+namespace Kentor.AuthServices.AspNetCore
+{
+    public class KentorAuthServicesHandler : AuthenticationHandler<KentorAuthServicesOptions>
+    {
+        /// <summary>
+        /// Creates a handler instance for use when processing a request.
+        /// </summary>
+        /// <returns>Handler instance.</returns>
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kentor.AuthServices.AspNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kentor.AuthServices.AspNetCore")]
+[assembly: AssemblyDescription("Saml2 Authentication ASP.NET Core Middleware")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Kentor.AuthServices.AspNetCore")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4f5a9e4-296d-4619-90bd-c939a03dcd2a")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("Kentor.AuthServices.Tests")]
