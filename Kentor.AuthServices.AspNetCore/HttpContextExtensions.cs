c357186cb4115ab34229f9b7d6789cdab20e05df
Kyle Spearrin <kspearrin@imobile3.com>
2016-05-23 11:35:32 -0400

    Converted command result extensions, owin context extensions, and MultipleIdentityAuthenticationTicket from OWIN. Added methods from OWIN auth handler and began conversion process for those.

---
+++ Kentor.AuthServices.AspNetCore/HttpContextExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Kentor.AuthServices.WebSso;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices.AspNetCore
+{
+    static class HttpContextExtensions
+    {
+        public async static Task<HttpRequestData> ToHttpRequestData(
+            this HttpContext context,
+            Func<byte[], byte[]> cookieDecryptor)
+        {
+            if(context == null)
+            {
+                return null;
+            }
+
+            IEnumerable<KeyValuePair<string, string[]>> formData = null;
+            if(context.Request.Body != null)
+            {
+                var formCollection = await context.Request.ReadFormAsync();
+                formData = formCollection.Select(d => new KeyValuePair<string, string[]>(d.Key, d.Value.ToArray()));
+            }
+
+            var applicationRootPath = context.Request.PathBase.Value;
+            if(string.IsNullOrEmpty(applicationRootPath))
+            {
+                applicationRootPath = "/";
+            }
+
+            return new HttpRequestData(
+                context.Request.Method,
+                new Uri(context.Request.Host.ToString()), // TODO, correct uri from Request
+                applicationRootPath,
+                formData,
+                context.Request.Cookies,
+                cookieDecryptor);
+        }
+    }
+}
