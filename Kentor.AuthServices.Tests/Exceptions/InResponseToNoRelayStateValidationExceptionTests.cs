d922f47353607a49892dc31e207709d1edb8c405
Albin Sunnanbo <albin@sunnanbo.com>
2016-06-17 12:18:29 +0200

    Create a separate InResponseToNoRelayStateValidationException to be able to detect the specific kind of replay detection from client code
    Fixes #504

---
+++ Kentor.AuthServices.Tests/Exceptions/InResponseToNoRelayStateValidationExceptionTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Kentor.AuthServices.Exceptions;
+
+namespace Kentor.AuthServices.Tests.Exceptions
+{
+    [TestClass]
+    public class InResponseToNoRelayStateValidationExceptionTests
+    {
+        [TestMethod]
+        public void InResponseToNoRelayStateValidationException_DefaultCtor()
+        {
+            ExceptionTestHelpers.TestDefaultCtor<InResponseToNoRelayStateValidationException>();
+        }
+
+        [TestMethod]
+        public void InResponseToNoRelayStateValidationException_SerializationCtor()
+        {
+            ExceptionTestHelpers.TestSerializationCtor<InResponseToNoRelayStateValidationException>();
+        }
+
+        [TestMethod]
+        public void InResponseToNoRelayStateValidationException_StringCtor()
+        {
+            var msg = "Message!";
+            var subject = new InResponseToNoRelayStateValidationException(msg);
+
+            subject.Message.Should().Be(msg);
+        }
+
+        [TestMethod]
+        public void InResponseToNoRelayStateValidationException_InnerExceptionCtor()
+        {
+            var msg = "Message!";
+            var innerException = new InvalidOperationException("InnerMessage!");
+
+            var subject = new InResponseToNoRelayStateValidationException(msg, innerException);
+
+            subject.Message.Should().Be("Message!");
+            subject.InnerException.Message.Should().Be("InnerMessage!");
+        }
+    }
+}
