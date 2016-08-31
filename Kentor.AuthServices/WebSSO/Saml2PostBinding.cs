8437121e9e7a950b649eeaea6486bf8a8b571b5f
Jimmy Tønners <jimmy@baguit.com>
2016-08-31 14:14:59 +0200

    Add support for different signing algorithms per identityprovider Tested with SAML2 Redirect and POST bindings

--- Kentor.AuthServices/WebSSO/Saml2PostBinding.cs
+++ Kentor.AuthServices/WebSSO/Saml2PostBinding.cs
@@ -52,13 +52,13 @@ namespace Kentor.AuthServices.WebSso
 
         public override CommandResult Bind(ISaml2Message message)
         {
-            if (message == null)
+            if(message == null)
             {
                 throw new ArgumentNullException(nameof(message));
             }
 
             var xml = message.ToXml();
-            if (message.SigningCertificate != null)
+            if(message.SigningCertificate != null)
             {
                 var xmlDoc = new XmlDocument()
                 {
@@ -81,9 +81,20 @@ namespace Kentor.AuthServices.WebSso
 
             var encodedXml = Convert.ToBase64String(Encoding.UTF8.GetBytes(xml));
 
-            var relayStateHtml = string.IsNullOrEmpty(message.RelayState) ? null : string.Format(CultureInfo.InvariantCulture, PostHtmlRelayStateFormatString, message.RelayState);
+            var relayStateHtml = string.IsNullOrEmpty(message.RelayState) ? null 
+                : string.Format(CultureInfo.InvariantCulture, PostHtmlRelayStateFormatString, message.RelayState);
 
-            var cr = new CommandResult() {ContentType = "text/html", Content = String.Format(CultureInfo.InvariantCulture, PostHtmlFormatString, message.DestinationUrl, relayStateHtml, message.MessageName, encodedXml)};
+            var cr = new CommandResult()
+            {
+                ContentType = "text/html",
+                Content = String.Format(
+                    CultureInfo.InvariantCulture, 
+                    PostHtmlFormatString, 
+                    message.DestinationUrl, 
+                    relayStateHtml, 
+                    message.MessageName, 
+                    encodedXml)
+            };
 
             return cr;
         }
