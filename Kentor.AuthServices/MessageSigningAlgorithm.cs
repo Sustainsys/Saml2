f9a2801b502be47b10ca07cf92fc95236cbdcf4e
Jimmy Tønners <jimmy@baguit.com>
2016-08-31 14:14:59 +0200

    Add support for different signing algorithms per identityprovider Tested with SAML2 Redirect and POST bindings

---
+++ Kentor.AuthServices/MessageSigningAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿namespace Kentor.AuthServices.Saml2P
+{
+    /// <summary>
+    /// Enum MessageSigningAlgorithm
+    /// </summary>
+    public enum MessageSigningAlgorithm
+    {
+        /// <summary>
+        /// The rsasha1
+        /// </summary>
+        RsaSecureHashAlgorithm1,
+        /// <summary>
+        /// The rsasha256
+        /// </summary>
+        RsaSecureHashAlgorithm256,
+        /// <summary>
+        /// The rsasha384
+        /// </summary>
+        RsaSecureHashAlgorithm384,
+        /// <summary>
+        /// The rsasha512
+        /// </summary>
+        RsaSecureHashAlgorithm512
+
+    }
+}
