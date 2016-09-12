604c977984cd5176769435a91f4d37a26f1d1906
Albin Sunnanbo <albin.sunnanbo@kentor.se>
2016-09-12 18:23:04 +0200

    Add LazyDictionaryCollection to wrap an IEnumerable<KeyValuePair> in an IReadOnlyDictionary interface

---
+++ Kentor.AuthServices.Tests/Internal/LazyDictionaryCollectionTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kentor.AuthServices.Internal;
+using System.Collections.Generic;
+using FluentAssertions;
+using System.Linq;
+
+namespace Kentor.AuthServices.Tests.Internal
+{
+    [TestClass]
+    public class LazyDictionaryCollectionTests
+    {
+        KeyValuePair<string, string[]>[] testDataSingleKeySingleValue = {
+            new KeyValuePair<string, string[]>("a", new[] {"b"}),
+        };
+        KeyValuePair<string, string[]>[] testDataSingleKeyMultipleValues = {
+            new KeyValuePair<string, string[]>("a", new[] {"b", "c"}),
+        };
+        KeyValuePair<string, string[]>[] testDataMultipleKeySingleValue = {
+            new KeyValuePair<string, string[]>("a", new[] {"b"}),
+            new KeyValuePair<string, string[]>("c", new[] {"d"}),
+        };
+        KeyValuePair<string, string[]>[] testDataDuplicateSecondsKeySingleValue = {
+            new KeyValuePair<string, string[]>("a", new[] {"b"}),
+            new KeyValuePair<string, string[]>("c", new[] {"d"}),
+            new KeyValuePair<string, string[]>("c", new[] {"e"}),
+        };
+
+        [TestMethod]
+        public void SingleKeySingleValueShouldRetrieveOkByKey()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            dictionary["a"].Should().Be("b");
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueShouldThrowKeyNotFoundExceptionWhenNoKeyFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            Action action = () =>
+            {
+                var x = dictionary["x"];
+            };
+            action.ShouldThrow<KeyNotFoundException>();
+        }
+
+        [TestMethod]
+        public void DictionaryCountShouldReturnCorrectValue()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataMultipleKeySingleValue);
+            dictionary.Count.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void KeysPropertyShouldReturnAllKeys()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataMultipleKeySingleValue);
+            dictionary.Keys.Should().BeEquivalentTo(new[] { "a", "c" });
+        }
+
+
+        [TestMethod]
+        public void ValuesPropertyShouldReturnAllValues()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataMultipleKeySingleValue);
+            dictionary.Values.Should().BeEquivalentTo(new[] { "b", "d" });
+        }
+
+        [TestMethod]
+        public void ValuesPropertyShouldThrowExceptionOnMultipleValues()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeyMultipleValues);
+            Action action = () =>
+            {
+                var x = dictionary.Values.ToList();
+            };
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueContainsKeyShouldReturnTrueWhenFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            dictionary.ContainsKey("a").Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueContainsKeyShouldReturnFalseWhenNotFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            dictionary.ContainsKey("x").Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueTryGetValueKeyShouldReturnTrueWhenFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            string item;
+            dictionary.TryGetValue("a", out item).Should().BeTrue();
+            item.Should().Be("b");
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueTryGetValueShouldReturnFalseWhenNotFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            string item;
+            dictionary.TryGetValue("b", out item).Should().BeFalse();
+            item.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueInDictionaryWithOtherMultipleKeysTryGetValueKeyShouldReturnTrueWhenFound()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataDuplicateSecondsKeySingleValue);
+            string item;
+            dictionary.TryGetValue("a", out item).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void MultipleKeyShouldThrowException()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataDuplicateSecondsKeySingleValue);
+            string item;
+            Action action = () =>
+            {
+                var x = dictionary.TryGetValue("c", out item);
+            };
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void SingleKeySingleValueInDictionaryWithOtherMultipleKeyShouldRetrieveOk()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataDuplicateSecondsKeySingleValue);
+            dictionary["a"].Should().Be("b");
+        }
+
+        [TestMethod]
+        public void MultipleKeyShouldThrowExceptionWhenRetrievedByIndexer()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataDuplicateSecondsKeySingleValue);
+            Action action = () =>
+            {
+                var x = dictionary["c"];
+            };
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public void EnumeratorShouldReturnAllKeysAndValues()
+        {
+            var dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            var hits = 0;
+            foreach (var kvp in dictionary)
+            {
+                kvp.Key.Should().Be("a");
+                kvp.Value.Should().Be("b");
+
+                hits++;
+            }
+            hits.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void PlainEnumeratorShouldReturnAllKeysAndValues()
+        {
+            System.Collections.IEnumerable dictionary = new LazyDictionaryCollection<string, string>(testDataSingleKeySingleValue);
+            var hits = 0;
+            foreach (KeyValuePair<string, string> kvp in dictionary)
+            {
+                kvp.Key.Should().Be("a");
+                kvp.Value.Should().Be("b");
+
+                hits++;
+            }
+            hits.Should().Be(1);
+        }
+    }
+}
---
+++ Kentor.AuthServices/Internal/LazyDictionaryCollection.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kentor.AuthServices.Internal
+{
+    /// <summary>
+    /// Wrapper to expose an IEnumerable&lt;KeyValuePair&gt; as a IReadOnlyDictionary
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the read-only dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the read-only dictionary.</typeparam>
+    internal class LazyDictionaryCollection<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        private readonly IEnumerable<KeyValuePair<TKey, TValue[]>> valueData;
+
+        /// <summary>
+        /// Initializes the LazyDictionary with data
+        /// </summary>
+        /// <param name="valueData"></param>
+        public LazyDictionaryCollection(IEnumerable<KeyValuePair<TKey, TValue[]>> valueData)
+        {
+            this.valueData = valueData;
+        }
+
+        /// <summary>
+        /// Gets the element that has the specified key in the read-only dictionary.
+        /// </summary>
+        /// <param name="key">Search key</param>
+        /// <returns></returns>
+        public TValue this[TKey key]
+        {
+            get
+            {
+                var valuesByKey = valueData.Where(item => key.Equals(item.Key));
+                if (!valuesByKey.Any())
+                {
+                    throw new KeyNotFoundException(string.Format(CultureInfo.CurrentCulture, "Key {0} not found", key));
+                }
+                return valuesByKey.Single().Value.Single();
+            }
+        }
+
+        /// <summary>
+        /// The number of items in the dictionary
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return valueData.Count();
+            }
+        }
+
+        /// <summary>
+        /// All keys in the dictionary
+        /// </summary>
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                return valueData.Select(d => d.Key);
+            }
+        }
+
+        /// <summary>
+        /// All Values in the dictionary
+        /// </summary>
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                return valueData.Select(d => d.Value.Single());
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the read-only dictionary contains an element that has the
+        /// specified key.
+        /// </summary>
+        /// <param name="key">The key to locate.</param>
+        /// <returns>
+        /// true if the read-only dictionary contains an element that has the specified key;
+        /// otherwise, false.
+        /// </returns>
+        public bool ContainsKey(TKey key)
+        {
+            return valueData.Any(d => key.Equals(d.Key));
+        }
+
+        /// <summary>
+        /// Gets an enumerator for the content
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return valueData.Select(d => new KeyValuePair<TKey, TValue>(d.Key, d.Value.Single())).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Gets the value that is associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key to locate.</param>
+        /// <param name="value">
+        /// When this method returns, the value associated with the specified key, if the
+        /// key is found; otherwise, the default value for the type of the value parameter.
+        /// This parameter is passed uninitialized.</param>
+        /// <returns>
+        /// true if the object that implements the System.Collections.Generic.IReadOnlyDictionary`2
+        /// interface contains an element that has the specified key; otherwise, false.
+        /// </returns>
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var valuesByKey = valueData.Where(item => key.Equals(item.Key));
+            if (!valuesByKey.Any())
+            {
+                value = default(TValue);
+                return false;
+            }
+            value = valuesByKey.Single().Value.Single();
+            return true;
+        }
+
+        /// <summary>
+        /// Gets an enumerator for the content
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
