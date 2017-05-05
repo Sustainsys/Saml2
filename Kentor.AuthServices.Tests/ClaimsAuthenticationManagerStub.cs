b732acfa2f4c8b9b3a7572be0e92e58d38d97639
Anders Abel <anders@abel.nu>
2017-05-05 16:16:28 +0200

    Null check name identifier when logging - Fixes #706

--- Kentor.AuthServices.Tests/ClaimsAuthenticationManagerStub.cs
+++ Kentor.AuthServices.Tests/ClaimsAuthenticationManagerStub.cs
@@ -9,9 +9,17 @@ namespace Kentor.AuthServices.Tests
 {
     class ClaimsAuthenticationManagerStub : ClaimsAuthenticationManager
     {
+        public bool ClearNameIdentifier { get; set; } = false;
+
         public override ClaimsPrincipal Authenticate(string resourceName, ClaimsPrincipal incomingPrincipal)
         {
             var newPrincipal = new ClaimsPrincipal(incomingPrincipal);
+
+            if(ClearNameIdentifier)
+            {
+                newPrincipal.Identities.First().RemoveClaim(
+                    newPrincipal.FindFirst(ClaimTypes.NameIdentifier));
+            }
             
             var id = new ClaimsIdentity("ClaimsAuthenticationManager");
             id.AddClaim(new Claim(ClaimTypes.Role, "RoleFromClaimsAuthManager", null, "ClaimsAuthenticationManagerStub"));
