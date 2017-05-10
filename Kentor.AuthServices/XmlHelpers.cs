04df587e92cd00b590beac4c3db7d28fda9a56cf
Steve Patches <steve@careevolution.com>
2017-05-10 11:50:05 -0400

    Fix message for unsupported Sha256
    Fixes #717
    
    This can't be covered by unit tests because those target 4.6.2.

--- Kentor.AuthServices/XmlHelpers.cs
+++ Kentor.AuthServices/XmlHelpers.cs
@@ -270,8 +270,6 @@ namespace Kentor.AuthServices
         {
             FixSignatureIndex(signedXml, signatureElement);
 
-            CheckSha256Support(signedXml);
-
             foreach (var keyIdentifier in signingKeys)
             {
                 var key = ((AsymmetricSecurityKey)keyIdentifier.CreateKey())
@@ -299,9 +297,9 @@ namespace Kentor.AuthServices
             new Lazy<object>(() => CryptoConfig.CreateFromName(Options.RsaSha256Uri));
 
         [ExcludeFromCodeCoverage]
-        private static void CheckSha256Support(SignedXml signedXml)
+        private static void CheckSha256Support(string signatureMethod)
         {
-            if (signedXml.SignatureMethod == Options.RsaSha256Uri
+            if (signatureMethod == Options.RsaSha256Uri
                 && rsaSha256Algorithm.Value == null)
             {
                 throw new InvalidSignatureException("SHA256 signatures require the algorithm to be registered at the process level. Upgrade to .Net 4.6.2 or call Kentor.AuthServices.Configuration.Options.GlobalEnableSha256XmlSignatures() on startup to register.");
@@ -390,6 +388,7 @@ namespace Kentor.AuthServices
             string minIncomingSignatureAlgorithm)
         {
             var signatureMethod = signedXml.SignedInfo.SignatureMethod;
+            CheckSha256Support(signatureMethod);
             ValidateSignatureMethodStrength(minIncomingSignatureAlgorithm, signatureMethod);
 
             ValidateReference(signedXml, xmlElement, GetCorrespondingDigestAlgorithm(minIncomingSignatureAlgorithm));
