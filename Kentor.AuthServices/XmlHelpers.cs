d1dede53781ec1dc98b11914854524010da00705
Steve Patches <steve@careevolution.com>
2017-05-11 11:16:25 -0400

    Improve error message for weak signing algorithm
    Fixes #720

--- Kentor.AuthServices/XmlHelpers.cs
+++ Kentor.AuthServices/XmlHelpers.cs
@@ -401,6 +401,7 @@ namespace Kentor.AuthServices
         /// <param name="minIncomingSignatureAlgorithm"></param>
         /// <param name="signatureMethod"></param>
         /// <exception cref="InvalidSignatureException">If the signaturemethod is too weak.</exception>
+        [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "minIncomingSigningAlgorithm")]
         public static void ValidateSignatureMethodStrength(
             string minIncomingSignatureAlgorithm,
             string signatureMethod)
@@ -410,7 +411,8 @@ namespace Kentor.AuthServices
             {
                 throw new InvalidSignatureException(
                     "The signing algorithm " + signatureMethod +
-                    " is weaker than the minimum accepted " + minIncomingSignatureAlgorithm + ".");
+                    " is weaker than the minimum accepted " + minIncomingSignatureAlgorithm +
+                    ". If you want to allow this signing algorithm, use the minIncomingSigningAlgorithm configuration attribute.");
             }
         }
 
