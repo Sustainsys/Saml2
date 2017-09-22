8552d0d4477bb18d1ea93d66995a75f8af5175c8
Anders Abel <anders.abel@sustainsys.com>
2017-09-22 16:17:54 +0200

    Asp.NET Core logging - Move AspNetCore2 test project to sub directory

--- Sustainsys.Saml2.AspNetCore2/PostConfigureSaml2Options.cs
+++ Sustainsys.Saml2.AspNetCore2/PostConfigureSaml2Options.cs
@@ -5,11 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Sustainsys.Saml2.AspNetCore2
 {
     class PostConfigureSaml2Options : IPostConfigureOptions<Saml2Options>
     {
+        private ILoggerFactory loggerFactory;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="loggerFactory">Logger factory to use to hook up Saml2 loggin.</param>
+        public PostConfigureSaml2Options(ILoggerFactory loggerFactory)
+        {
+            this.loggerFactory = loggerFactory;
+        }
+
         public void PostConfigure(string name, Saml2Options options)
         {
             if(options == null)
@@ -17,7 +29,16 @@ namespace Sustainsys.Saml2.AspNetCore2
                 throw new ArgumentNullException(nameof(options));
             }
 
-            options.SPOptions.Logger = options.SPOptions.Logger ?? new NullLoggerAdapter();
+            if(loggerFactory != null)
+            {
+                options.SPOptions.Logger = new AspNetCoreLoggerAdapter(
+                    loggerFactory.CreateLogger<Saml2Handler>());
+            }
+            else
+            {
+                options.SPOptions.Logger = new NullLoggerAdapter();
+            }
+            options.SPOptions.Logger.WriteVerbose("Saml2 logging enabled.");
         }
     }
 }
---
+++ Tests/Sustainsys.Saml2.AspNetCore2.Tests/PostConfigureSaml2OptionsTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System;
+using NSubstitute;
+using Microsoft.Extensions.Logging;
+using Kentor.AuthServices;
+
+namespace Sustainsys.Saml2.AspNetCore2.Tests
+{
+    [TestClass]
+    public class PostConfigureSaml2OptionsTests
+    {
+        [TestMethod]
+        public void PostConfigureSaml2Options_PostConfigure_Nullcheck()
+        {
+            var subject = new PostConfigureSaml2Options(null);
+
+            subject.Invoking(s => s.PostConfigure(null, null))
+                .ShouldThrow<ArgumentNullException>().
+                And.ParamName.Should().Be("options");
+        }
+
+        [TestMethod]
+        public void PostConfigureSaml2Options_PostConfigure_Logger()
+        {
+            var options = new Saml2Options();
+
+            options.SPOptions.Logger.Should().BeNull("Precondition");
+
+            var loggerFactory = Substitute.For<ILoggerFactory>();
+            var logger = new MockLogger();
+            loggerFactory.CreateLogger<Saml2Handler>().Returns(logger);
+
+            var subject = new PostConfigureSaml2Options(loggerFactory);
+
+            subject.PostConfigure(null, options);
+
+            logger.ReceivedLogLevel.Should().Be(LogLevel.Debug);
+            logger.ReceivedMessage.Should().Match("*enabled*");
+        }
+
+        [TestMethod]
+        public void PostConfigureSaml2Options_PostConfigure_NullLoggerFactoryGivesNullLogger()
+        {
+            var options = new Saml2Options();
+
+            options.SPOptions.Logger.Should().BeNull("Precondition");
+
+            var subject = new PostConfigureSaml2Options(null);
+
+            subject.PostConfigure(null, options);
+
+            options.SPOptions.Logger.Should().BeOfType<NullLoggerAdapter>();
+        }
+    }
+}
