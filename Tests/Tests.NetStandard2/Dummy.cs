615504bfe928cd3965ffddb268d74df24d57a602
Anders Abel <anders.abel@sustainsys.com>
2017-11-07 09:18:24 +0100

    Added Netstandard and shared tests

---
+++ Tests/Tests.NetStandard2/Dummy.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.NetStandard2
+{
+    [TestClass]
+    public class Dummy
+    {
+        [TestMethod]
+        public void DummyTest()
+        {
+               
+        }
+    }
+}
