435d220fc58440db85cadc9fdc7886109f79be14
Anders Abel <anders.abel@sustainsys.com>
2017-11-29 13:28:28 +0100

    Added Sample IdentityServer4 project

---
+++ Samples/SampleIdentityServer4/MyApi.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleIdentityServer4
+{
+    [Route("myapi")]
+    public class MyApi : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        // TODO: Set Authentication Scheme to JWT handler.
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
---
+++ Samples/SampleIdentityServer4/Startup.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace SampleIdentityServer4
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(GetApiResources())
+                .AddInMemoryClients(GetClients())
+                .AddTestUsers(GetUsers());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            
+            app.UseIdentityServer();
+            
+            app.UseMvc();
+        }
+
+        static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myapi", "My API")
+            };
+        }
+
+        static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "js-client",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "myapi" }
+                }
+            };
+        }
+
+        static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username ="alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+        }
+    }
+}
