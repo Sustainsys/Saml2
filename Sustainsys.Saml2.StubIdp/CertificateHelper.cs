d504885034144bacd054cf80564db55931016099
Anders Abel <anders.abel@sustainsys.com>
2017-12-14 13:47:19 +0100

    Change Stubidp cert to Sustainsys name

--- Sustainsys.Saml2.StubIdp/CertificateHelper.cs
+++ Sustainsys.Saml2.StubIdp/CertificateHelper.cs
@@ -11,17 +11,58 @@ namespace Kentor.AuthServices.StubIdp
 {
     public class CertificateHelper
     {
+        public static X509Certificate2 SigningCertificate
+        {
+            get
+            {
+                // If accessing on the legacy stubidp.kentor.se host name, use old certificate
+                // to not break existing configured clients.
+                if(HttpContext.Current.Request.Url.Host == "stubidp.kentor.se")
+                {
+                    return signingCertificateKentor;
+                }
+                return signingCertificate;
+            }
+        }
+
         // The X509KeyStorageFlags.MachineKeySet flag is required when loading a
         // certificate from file on a shared hosting solution such as Azure.
-        public static readonly X509Certificate2 SigningCertificate =
-            new X509Certificate2(
+        private static readonly X509Certificate2 signingCertificateKentor
+            = new X509Certificate2(
                 HttpContext.Current.Server.MapPath(
                 "~\\App_Data\\Kentor.AuthServices.StubIdp.pfx"), "",
                 X509KeyStorageFlags.MachineKeySet);
 
-        public static readonly KeyDescriptor SigningKey = 
+        private static readonly X509Certificate2 signingCertificate
+            = new X509Certificate2(
+                HttpContext.Current.Server.MapPath(
+                "~\\App_Data\\stubidp.sustainsys.com.pfx"), "",
+                X509KeyStorageFlags.MachineKeySet);
+
+        public static KeyDescriptor SigningKey
+        {
+            get
+            {
+                // If accessing on the legacy stubidp.kentor.se host name, use old certificate
+                // to not break existing configured clients.
+                if (HttpContext.Current.Request.Url.Host == "stubidp.kentor.se")
+                {
+                    return signingKeyKentor;
+                }
+                return signingKey;
+
+            }
+        }
+
+        private static readonly KeyDescriptor signingKeyKentor = 
             new KeyDescriptor(
             new SecurityKeyIdentifier(
-                (new X509SecurityToken(SigningCertificate)).CreateKeyIdentifierClause<X509RawDataKeyIdentifierClause>()));
+                (new X509SecurityToken(signingCertificateKentor)).CreateKeyIdentifierClause<X509RawDataKeyIdentifierClause>()));
+
+        private static readonly KeyDescriptor signingKey =
+            new KeyDescriptor(
+            new SecurityKeyIdentifier(
+                (new X509SecurityToken(signingCertificate)).CreateKeyIdentifierClause<X509RawDataKeyIdentifierClause>()));
+
     }
 }
