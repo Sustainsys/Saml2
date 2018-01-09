8cdd2eb9a82cb9aa3e238251396dbecd9cc6792e
Anders Abel <anders.abel@sustainsys.com>
2018-01-09 18:09:58 +0100

    Update package metadata for preview01

--- Sustainsys.Saml2/Properties/AssemblyInfo.cs
+++ Sustainsys.Saml2/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("Saml2 Authentication for ASP.NET")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
