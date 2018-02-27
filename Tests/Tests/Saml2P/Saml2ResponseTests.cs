782ac71fef58a7595a0dead745e29431f4a6fc7b
Anders Abel <anders.abel@sustainsys.com>
2018-02-27 22:10:01 +0100

    Add tests for Duo's new SAML2 attack - Fixes #921

--- Tests/Tests/Saml2P/Saml2ResponseTests.cs
+++ Tests/Tests/Saml2P/Saml2ResponseTests.cs
@@ -211,6 +211,77 @@ namespace Sustainsys.Saml2.Tests.Saml2P
             a.ShouldNotThrow();
         }
 
+        /// This is a specific test for the vulnerabilities found by Duo in February 2018
+        /// https://duo.com/blog/duo-finds-saml-vulnerabilities-affecting-multiple-implementations
+        [TestMethod]
+        public void Saml2Response_GetClaims_CorrectSignedResponseMessage_CommentInNameId()
+        {
+            var response =
+            @"<?xml version=""1.0"" encoding=""UTF-8""?>
+            <saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+            ID = """ + MethodBase.GetCurrentMethod().Name + @""" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z"">
+                <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                <saml2:Assertion xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                Version=""2.0"" ID=""" + MethodBase.GetCurrentMethod().Name + @"_Assertion1""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>Some<!--Comment-->User</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                    <saml2:Conditions NotOnOrAfter=""2100-01-01T00:00:00Z"" />
+                </saml2:Assertion>
+            </saml2p:Response>";
+
+            var signedResponse = SignedXmlHelper.SignXml(response);
+
+            Action a = () => Saml2Response.Read(signedResponse).GetClaims(StubFactory.CreateOptions());
+            a.ShouldThrow<XmlException>();
+        }
+
+        /// This is a specific test for the vulnerabilities found by Duo in February 2018
+        /// https://duo.com/blog/duo-finds-saml-vulnerabilities-affecting-multiple-implementations
+        [TestMethod]
+        public void Saml2Response_GetClaims_CorrectSignedResponseMessage_CommentInAttributeValue()
+        {
+            var response =
+            @"<?xml version=""1.0"" encoding=""UTF-8""?>
+            <saml2p:Response xmlns:saml2p=""urn:oasis:names:tc:SAML:2.0:protocol""
+            xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+            ID = """ + MethodBase.GetCurrentMethod().Name + @""" Version=""2.0"" IssueInstant=""2013-01-01T00:00:00Z"">
+                <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                <saml2p:Status>
+                    <saml2p:StatusCode Value=""urn:oasis:names:tc:SAML:2.0:status:Success"" />
+                </saml2p:Status>
+                <saml2:Assertion xmlns:saml2=""urn:oasis:names:tc:SAML:2.0:assertion""
+                Version=""2.0"" ID=""" + MethodBase.GetCurrentMethod().Name + @"_Assertion1""
+                IssueInstant=""2013-09-25T00:00:00Z"">
+                    <saml2:Issuer>https://idp.example.com</saml2:Issuer>
+                    <saml2:Subject>
+                        <saml2:NameID>SomeUser</saml2:NameID>
+                        <saml2:SubjectConfirmation Method=""urn:oasis:names:tc:SAML:2.0:cm:bearer"" />
+                    </saml2:Subject>
+                    <saml2:Conditions NotOnOrAfter=""2100-01-01T00:00:00Z"" />
+                    <saml2:AttributeStatement>
+                        <saml2:Attribute Name=""CommentTest"">
+                            <saml2:AttributeValue>Some<!--Comment-->Value</saml2:AttributeValue>
+                        </saml2:Attribute>
+                    </saml2:AttributeStatement>
+                </saml2:Assertion>
+            </saml2p:Response>";
+
+            var signedResponse = SignedXmlHelper.SignXml(response);
+
+            var claims = Saml2Response.Read(signedResponse).GetClaims(StubFactory.CreateOptions());
+
+            claims.Single().FindFirst("CommentTest").Value.Should().Be("SomeValue");
+        }
+
+
         [TestMethod]
         public void Saml2Response_GetClaims_CorrectSignedResponseMessageSecondaryKey()
         {
