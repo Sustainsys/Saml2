609d2b843b02451ca4acb326be617143bb7679aa
Steve Patches <steve@careevolution.com>
2018-03-26 17:12:36 -0400

    handle stored request state without returnurl during OWIN error response

--- Sustainsys.Saml2.Owin/Saml2AuthenticationHandler.cs
+++ Sustainsys.Saml2.Owin/Saml2AuthenticationHandler.cs
@@ -61,11 +61,18 @@ namespace Sustainsys.Saml2.Owin
         private AuthenticationTicket CreateErrorAuthenticationTicket(HttpRequestData httpRequestData, Exception ex)
         {
             AuthenticationProperties authProperties = null;
-            if (httpRequestData.StoredRequestState != null)
+            if (httpRequestData.StoredRequestState?.RelayData != null)
             {
                 authProperties = new AuthenticationProperties(
                     httpRequestData.StoredRequestState.RelayData);
+            }
+            else
+            {
+                authProperties = new AuthenticationProperties();
+            }
 
+            if (httpRequestData.StoredRequestState?.ReturnUrl != null)
+            {
                 // ReturnUrl is removed from AuthProps dictionary to save space, need to put it back.
                 authProperties.RedirectUri = httpRequestData.StoredRequestState.ReturnUrl.OriginalString;
             }
@@ -83,10 +90,7 @@ namespace Sustainsys.Saml2.Owin
 
                     redirectUrl = httpRequestData.ApplicationUrl;
                 }
-                authProperties = new AuthenticationProperties
-                {
-                    RedirectUri = redirectUrl.OriginalString
-                };
+                authProperties.RedirectUri = redirectUrl.OriginalString;
             }
 
             // The Google middleware adds this, so let's follow that example.
