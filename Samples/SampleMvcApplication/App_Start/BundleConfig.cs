bf5771c48bade13102fc6df982b6ca0ef5a780d3
Steve Patches <steve@careevolution.com>
2019-05-30 16:45:09 -0400

    remove modernizr

--- Samples/SampleMvcApplication/App_Start/BundleConfig.cs
+++ Samples/SampleMvcApplication/App_Start/BundleConfig.cs
@@ -11,11 +11,6 @@ namespace SampleMvcApplication
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
@@ -23,6 +18,7 @@ namespace SampleMvcApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
