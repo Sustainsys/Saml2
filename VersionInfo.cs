b0143b9debcaff5cb5ae243cdeebb5b0dac04c2c
Anders Abel <anders.abel@sustainsys.com>
2020-01-17 16:25:45 +0100

    Update for 1.0.1 release

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Sustainsys")]
-[assembly: AssemblyCopyright("Copyright © Sustainsys and contributors 2013-2018")]
+[assembly: AssemblyCopyright("Copyright © Sustainsys and contributors 2013-2020")]
 
 
 // Sustainsys.Saml2 uses semantic versioning
@@ -11,6 +11,6 @@ using System.Reflection;
 //      Patch Number
 //
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
