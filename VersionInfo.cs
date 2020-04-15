d05d7945751f73f55d44fde9796a7b848ed9eb6a
Anders Abel <anders.abel@sustainsys.com>
2020-04-15 14:26:48 +0200

    Update for release

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -11,6 +11,6 @@ using System.Reflection;
 //      Patch Number
 //
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2")]
