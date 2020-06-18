74045327e15812c60610746bd0632f1f9236b5bd
Anders Abel <anders.abel@sustainsys.com>
2020-06-18 14:57:36 +0200

    Allow duplicate localized entries - Fixes #1214

--- Sustainsys.Saml2.Metadata/LocalizedEntryCollection.cs
+++ Sustainsys.Saml2.Metadata/LocalizedEntryCollection.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Sustainsys.Saml2.Metadata
 {
-    public class LocalizedEntryCollection<T> : KeyedCollection<string, T> where T : LocalizedEntry
+    public class LocalizedEntryCollection<T>  : IEnumerable<T>
+        where T : LocalizedEntry
     {
-        protected override string GetKeyForItem(T item)
-        {
-            return item.Language;
-        }
+        private List<T> items = new List<T>();
+
+        public void Add(T item) => items.Add(item);
+
+        public void Clear() => items.Clear();
+
+        public int Count => items.Count;
+
+        public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => items.GetEnumerator();
     }
 }
