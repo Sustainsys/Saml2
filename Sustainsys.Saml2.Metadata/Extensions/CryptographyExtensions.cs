2310c5b5781b7be0b84dcf2d3d4e713de82e61d2
Dirk Lemstra <dirk@lemstra.org>
2020-11-14 11:22:56 +0100

    Removed unused methods.

--- Sustainsys.Saml2.Metadata/Extensions/CryptographyExtensions.cs
+++ Sustainsys.Saml2.Metadata/Extensions/CryptographyExtensions.cs
@@ -1,13 +1,5 @@
-﻿using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Cryptography.Xml;
-using System.Xml;
-using EncryptingCredentials = Microsoft.IdentityModel.Tokens.EncryptingCredentials;
-using SecurityAlgorithms = Microsoft.IdentityModel.Tokens.SecurityAlgorithms;
 
 namespace Sustainsys.Saml2.Metadata.Extensions
 {
@@ -15,121 +7,6 @@ namespace Sustainsys.Saml2.Metadata.Extensions
     // for not set to internal to prevent conflict
     internal static class CryptographyExtensions
     {
-        internal static void Encrypt(this XmlElement elementToEncrypt, EncryptingCredentials encryptingCredentials)
-        {
-            if (elementToEncrypt == null)
-            {
-                throw new ArgumentNullException(nameof(elementToEncrypt));
-            }
-            if (encryptingCredentials == null)
-            {
-                throw new ArgumentNullException(nameof(encryptingCredentials));
-            }
-
-            string enc;
-            int keySize;
-            switch (encryptingCredentials.Enc)
-            {
-                case SecurityAlgorithms.Aes128CbcHmacSha256:
-                    enc = EncryptedXml.XmlEncAES128Url;
-                    keySize = 128;
-                    break;
-
-                case SecurityAlgorithms.Aes192CbcHmacSha384:
-                    enc = EncryptedXml.XmlEncAES192Url;
-                    keySize = 192;
-                    break;
-
-                case SecurityAlgorithms.Aes256CbcHmacSha512:
-                    enc = EncryptedXml.XmlEncAES256Url;
-                    keySize = 256;
-                    break;
-
-                default:
-                    throw new CryptographicException(
-                        $"Unsupported cryptographic algorithm {encryptingCredentials.Enc}");
-            }
-
-            var encryptedData = new System.Security.Cryptography.Xml.EncryptedData
-            {
-                Type = EncryptedXml.XmlEncElementUrl,
-                EncryptionMethod = new System.Security.Cryptography.Xml.EncryptionMethod(enc)
-            };
-
-            string alg;
-            switch (encryptingCredentials.Alg)
-            {
-                case SecurityAlgorithms.RsaOAEP:
-                    alg = EncryptedXml.XmlEncRSAOAEPUrl;
-                    break;
-
-                case SecurityAlgorithms.RsaPKCS1:
-                    alg = EncryptedXml.XmlEncRSA15Url;
-                    break;
-
-                default:
-                    throw new CryptographicException(
-                        $"Unsupported cryptographic algorithm {encryptingCredentials.Alg}");
-            }
-            var encryptedKey = new EncryptedKey
-            {
-                EncryptionMethod = new System.Security.Cryptography.Xml.EncryptionMethod(alg),
-            };
-
-            var encryptedXml = new EncryptedXml();
-            byte[] encryptedElement;
-            using (var symmetricAlgorithm = new RijndaelManaged())
-            {
-                X509SecurityKey x509SecurityKey = encryptingCredentials.Key as X509SecurityKey;
-                if (x509SecurityKey == null)
-                {
-                    throw new CryptographicException(
-                        "The encrypting credentials have an unknown key of type {encryptingCredentials.Key.GetType()}");
-                }
-
-                symmetricAlgorithm.KeySize = keySize;
-                encryptedKey.CipherData = new System.Security.Cryptography.Xml.CipherData(EncryptedXml.EncryptKey(symmetricAlgorithm.Key,
-                    (RSA)x509SecurityKey.PublicKey, alg == EncryptedXml.XmlEncRSAOAEPUrl));
-                encryptedElement = encryptedXml.EncryptData(elementToEncrypt, symmetricAlgorithm, false);
-            }
-            encryptedData.CipherData.CipherValue = encryptedElement;
-
-            encryptedData.KeyInfo = new KeyInfo();
-            encryptedData.KeyInfo.AddClause(new KeyInfoEncryptedKey(encryptedKey));
-            EncryptedXml.ReplaceElement(elementToEncrypt, encryptedData, false);
-        }
-
-        internal static void Encrypt(this XmlElement elementToEncrypt, bool useOaep, X509Certificate2 certificate)
-        {
-            if (certificate == null) throw new ArgumentNullException(nameof(certificate));
-
-            var encryptedData = new System.Security.Cryptography.Xml.EncryptedData
-            {
-                Type = EncryptedXml.XmlEncElementUrl,
-                EncryptionMethod = new System.Security.Cryptography.Xml.EncryptionMethod(EncryptedXml.XmlEncAES256Url)
-            };
-
-            var algorithm = useOaep ? EncryptedXml.XmlEncRSAOAEPUrl : EncryptedXml.XmlEncRSA15Url;
-            var encryptedKey = new EncryptedKey
-            {
-                EncryptionMethod = new System.Security.Cryptography.Xml.EncryptionMethod(algorithm),
-            };
-
-            var encryptedXml = new EncryptedXml();
-            byte[] encryptedElement;
-            using (var symmetricAlgorithm = new RijndaelManaged())
-            {
-                symmetricAlgorithm.KeySize = 256;
-                encryptedKey.CipherData = new System.Security.Cryptography.Xml.CipherData(EncryptedXml.EncryptKey(symmetricAlgorithm.Key, (RSA)certificate.PublicKey.Key, useOaep));
-                encryptedElement = encryptedXml.EncryptData(elementToEncrypt, symmetricAlgorithm, false);
-            }
-            encryptedData.CipherData.CipherValue = encryptedElement;
-
-            encryptedData.KeyInfo = new KeyInfo();
-            encryptedData.KeyInfo.AddClause(new KeyInfoEncryptedKey(encryptedKey));
-            EncryptedXml.ReplaceElement(elementToEncrypt, encryptedData, false);
-        }
-
         internal static AsymmetricAlgorithm GetSha256EnabledAsymmetricAlgorithm(
             this AsymmetricAlgorithm original)
         {
@@ -167,30 +44,6 @@ namespace Sustainsys.Saml2.Metadata.Extensions
             {
                 cspParams.Flags = CspProviderFlags.UseMachineKeyStore;
             }
-        }
-
-        // CryptoConfig.CreateFromName doesn't know about these
-        private static Dictionary<string, Type> s_extraAlgorithms = new Dictionary<string, Type>(StringComparer.OrdinalIgnoreCase)
-        {
-            {  SecurityAlgorithms.RsaSha256Signature, typeof(ManagedRSASHA256SignatureDescription) },
-            {  SecurityAlgorithms.RsaSha384Signature, typeof(ManagedRSASHA384SignatureDescription) },
-            {  SecurityAlgorithms.RsaSha512Signature, typeof(ManagedRSASHA512SignatureDescription) }
-        };
-
-        public static object CreateAlgorithmFromName(string name, params object[] args)
-        {
-            var result = CryptoConfig.CreateFromName(name);
-            if (result != null)
-            {
-                return result;
-            }
-
-            Type type;
-            if (!s_extraAlgorithms.TryGetValue(name, out type))
-            {
-                throw new CryptographicException($"Unknown crypto algorithm '{name}'");
-            }
-            return Activator.CreateInstance(type, args);
         }
     }
 }
