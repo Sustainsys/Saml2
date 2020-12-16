8a5d201fe1dab9a1379a56cb5b3e41b8e3e92f71
Anders Abel <anders.abel@sustainsys.com>
2020-12-16 20:44:18 +0100

    Improve tests

--- Tests/Tests.Shared/Internal/RSAEncryptedXmlTests.cs
+++ Tests/Tests.Shared/Internal/RSAEncryptedXmlTests.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography.Xml;
 using System.Xml;
 using FluentAssertions;
 using Sustainsys.Saml2.Internal;
+using System.Linq;
 
 namespace Sustainsys.Saml2.Tests.Internal
 {
@@ -15,8 +16,8 @@ namespace Sustainsys.Saml2.Tests.Internal
         public void AesGcmNonceIsSupported()
         {
             var aesGcm128NonceSizeInBytes = RSAEncryptedXml.AesGcm128NonceSizeInBits / 8;
-            var dummyBytes = new byte[aesGcm128NonceSizeInBytes + 1];
-            var expectedIv = new byte[aesGcm128NonceSizeInBytes];
+            var dummyBytes = Enumerable.Range(42, aesGcm128NonceSizeInBytes + 1).Select(i => (byte)i).ToArray();
+            var expectedIv = Enumerable.Range(42, aesGcm128NonceSizeInBytes);
             var dummyData = new EncryptedData();
             dummyData.CipherData = new CipherData(dummyBytes);
             var rex = new RSAEncryptedXml(new XmlDocument(), null );
@@ -28,12 +29,13 @@ namespace Sustainsys.Saml2.Tests.Internal
         [TestMethod]
         public void NonAesGcmAlgorithmsAreHandledByBaseClass()
         {
-            var dummyBytes = new byte[20];
+            var dummyBytes = Enumerable.Range(17, 20).Select(i => (byte)i).ToArray();
             var dummyData = new EncryptedData();
             dummyData.CipherData = new CipherData(dummyBytes);
             var rex = new RSAEncryptedXml(new XmlDocument(), null );
             var iv = rex.GetDecryptionIV(dummyData, EncryptedXml.XmlEncAES256Url);
             iv.Should().NotBeNull();
+            iv.Should().BeEquivalentTo(Enumerable.Range(17, 16));
         }
     }
 }
