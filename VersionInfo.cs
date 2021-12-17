f3ad00b99948ba71baa1c1d0892526716ba368eb
Sami Saqer <sami@cmgecm.com>
2021-12-16 19:33:17 -0800

    fix copyright character

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("Sustainsys")]
-[assembly: AssemblyCopyright("Copyright � Sustainsys and contributors 2013-2020")]
+[assembly: AssemblyCopyright("Copyright © Sustainsys and contributors 2013-2020")]
 
 
 // Sustainsys.Saml2 uses semantic versioning
