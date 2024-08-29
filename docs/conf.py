be8c446d90cfa8f32756770d62b470af4b358f81
Anders Abel <anders.abel@sustainsys.com>
2024-08-29 20:05:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ todo_include_todos = False
 html_theme = 'sphinx_rtd_theme'
 
 def setup(app):
-    app.add_stylesheet('custom.css')
+    app.add_css_file('custom.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
