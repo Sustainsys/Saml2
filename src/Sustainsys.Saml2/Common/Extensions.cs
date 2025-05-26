15dfe1a9b0c3e349de5134ae3751f5f786594d82
Anders Abel <anders.abel@sustainsys.com>
2025-05-26 09:51:36 +0200

    Comment typo fix

--- src/Sustainsys.Saml2/Common/Extensions.cs
+++ src/Sustainsys.Saml2/Common/Extensions.cs
@@ -6,7 +6,7 @@
 public class Extensions
 {
     /// <summary>
-    /// Collection of conentent nodes read by extension-aware readers.
+    /// Collection of content nodes read by extension-aware readers.
     /// </summary>
     public List<object> Contents { get; set; } = [];
 }
