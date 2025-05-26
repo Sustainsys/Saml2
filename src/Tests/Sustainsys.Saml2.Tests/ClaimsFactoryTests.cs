c6f4234538c0521471a6165d0285d7c721e831b7
Anders Abel <anders.abel@sustainsys.com>
2025-05-26 10:03:01 +0200

    Test case for ClaimsFactory (failing)

---
+++ src/Tests/Sustainsys.Saml2.Tests/ClaimsFactoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Sustainsys.Saml2.Saml;
+using System.Security.Claims;
+
+namespace Sustainsys.Saml2.Tests;
+
+public class ClaimsFactoryTests
+{
+    [Fact]
+    public void GetClaimsIdentity()
+    {
+        var subject = new ClaimsFactory();
+
+        Assertion assertion = new()
+        {
+            Subject = new()
+            {
+                NameId = new()
+                {
+                    Value = "NameIdValue"
+                }
+            },
+            Attributes =
+            {
+                // Single valued attribute
+                new()
+                {
+                    Name = "email",
+                    Values = { "john.doe@example.com" }
+                },
+                // Multi valued attribute
+                new()
+                {
+                    Name = "role",
+                    Values = { "admin", "boss" }
+                }
+                // TODO: Test for multiple attribute statements with same attribute name
+            }
+        };
+
+        var actual = subject.GetClaimsIdentity(assertion);
+
+        Claim[] expectedClaims =
+            [
+                new(ClaimTypes.NameIdentifier, "NameIdValue"),
+                new("email", "john.doe@example.com"),
+                new("role", "admin"),
+                new("role", "boss")
+            ];
+
+        actual.Claims.Should().BeEquivalentTo(expectedClaims);
+    }
+}
