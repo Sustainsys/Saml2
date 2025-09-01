643d6e7381b93a6552ba7ef84c5bbb0367f89b12
Anders Abel <anders.abel@sustainsys.com>
2025-09-01 10:06:41 +0200

    Test for validated

--- src/Tests/Sustainsys.Saml2.Tests/Validation/ValidatedTests.cs
+++ src/Tests/Sustainsys.Saml2.Tests/Validation/ValidatedTests.cs
@@ -1,6 +1,44 @@
-﻿namespace Sustainsys.Saml2.Tests.Validation;
+﻿using Sustainsys.Saml2.Tests.Helpers;
+using Sustainsys.Saml2.Validation;
+
+namespace Sustainsys.Saml2.Tests.Validation;
 public class ValidatedTests
 {
-    [Fact(Skip = "TODO: Implement tests for Validated")]
-    public void ValidatedTestsMissing() { }
+    private class MyClass
+    {
+        public object Property { get; set; }
+    }
+
+    private static Validated<MyClass> CreateSubject(out MyClass data)
+    {
+        data = new()
+        {
+            Property = new()
+        };
+        object pars = new();
+        NoOpValidator<MyClass> validator = new();
+
+        return data.Validate(validator, pars);
+    }
+
+    [Fact]
+    public void Validate()
+    {
+        var validated = CreateSubject(out var data);
+
+        validated.Value.Should().BeSameAs(data);
+        MyClass implicitConvert = validated;
+        implicitConvert.Should().BeSameAs(data);
+    }
+
+
+    [Fact]
+    public void GetValidated()
+    {
+        var validated = CreateSubject(out MyClass data);
+
+        var validatedProperty = validated.GetValidated(c => c.Property);
+
+        validatedProperty.Value.Should().BeSameAs(data.Property);
+    }
 }
