9e08c00ca33d3fe2c651655d25516f15d99f52fd
Anders Abel <anders.abel@sustainsys.com>
2025-09-25 22:49:27 +0200

    Make sure all tests work

--- src/Tests/Sustainsys.Saml2.Tests/Validation/ResponseValidatorTests.cs
+++ src/Tests/Sustainsys.Saml2.Tests/Validation/ResponseValidatorTests.cs
@@ -42,6 +42,7 @@ public class ResponseValidatorTests
                             },
                         }
                     },
+                    TrustLevel = TrustLevel.TLS
                 }
             },
         };
