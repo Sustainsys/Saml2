7a142ed568ca18346535da6540f9cd9594213681
Anders Abel <anders.abel@sustainsys.com>
2026-04-30 10:51:14 +0200

    Move CSP to HTTP header and add CacheControl

--- src/foss/Sustainsys.Saml2/Bindings/HttpPostBinding.cs
+++ src/foss/Sustainsys.Saml2/Bindings/HttpPostBinding.cs
@@ -145,22 +145,19 @@ public class HttpPostBinding() : FrontChannelBinding(Constants.BindingUris.HttpP
                     encodedXml);
 
         httpResponse.ContentType = "text/html";
+        httpResponse.Headers.ContentSecurityPolicy = "default-src none; script-src 'sha256-H3SVZBYrbqBt3ncrT/nNmOb6nwCjC12cPQzh5jnW4Y0='";
+        httpResponse.Headers.CacheControl = "no-cache, no-store";
         await httpResponse.WriteAsync(content);
     }
 
     private readonly CompositeFormat PostHtmlRelayStateFormatString = CompositeFormat.Parse(@"
 <input type=""hidden"" name=""RelayState"" value=""{0}""/>");
 
-    // TODO: Move CSP to HTTP header
-    // TODO: Set cache-control no-cache, no-store (Bindings 3.5.5.1)
     private readonly CompositeFormat PostHtmlFormatString =
         CompositeFormat.Parse(@"<?xml version=""1.0"" encoding=""UTF-8""?>
 <!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.1//EN""
 ""http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"">
 <html xmlns=""http://www.w3.org/1999/xhtml"" xml:lang=""en"">
-<head>
-<meta http-equiv=""Content-Security-Policy"" content=""script-src 'sha256-H3SVZBYrbqBt3ncrT/nNmOb6nwCjC12cPQzh5jnW4Y0='"">
-</head>
 <body>
 <noscript>
 <p>
